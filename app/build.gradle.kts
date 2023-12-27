bf4cafd7eafe87720a84ceec97cffb1b018dde75
cormacwit <20093840@mail.wit.ie>
2023-12-27 14:20:54 +0000

    Rooms successfully imported using imports from site
    Moving development to use us rooms system
    https://developer.android.com/training/data-storage/room#kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,12 +70,40 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    testImplementation(kotlin("test"))
+    implementation("org.slf4j:slf4j-simple:1.6.1")
+    implementation("io.github.microutils:kotlin-logging:1.6.22")
+    implementation("com.google.code.gson:gson:2.8.9")
     //Room
-    //implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
-    //ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
-    //implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
-    //implementation ("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    val room_version = "2.6.1"
 
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 
+    // To use Kotlin annotation processing tool (kapt)
+    //kapt("androidx.room:room-compiler:$room_version")
+    // To use Kotlin Symbol Processing (KSP)
+    //ksp("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
 }
+
+
+
 
